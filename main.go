fa6f7d594e60fdf56f77fbd9247b52d0d21232dc
achunchoshadrack <achunchoshadrack@gmail.com>
2023-11-28 12:04:13 -0800

    Desh

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ func main(){
     var name1 string
 	fmt.Println("enter your age")
 	fmt.Scanln(&numOne)
-	fmt.Println("enter your age")
+	fmt.Println("enter your name")
 	fmt.Scanln(&name1)
 	fmt.Println("my name is ", name1,"and my age is", numOne)
+	//printf string formating
+	fmt.Printf("my name is %v and my age is %v \n", name1, numOne)
+	fmt.Printf("my name is %q and my age is %q \n", name1, numOne)
+	fmt.Printf("my name is of type %T\n", name1)
+	//sprintf formated string
+	
 }
