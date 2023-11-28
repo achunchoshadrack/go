5f853aba89751307af16ba250657c21b740f22cd
achunchoshadrack <achunchoshadrack@gmail.com>
2023-11-28 12:31:55 -0800

    desh

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ func main(){
 	// bits and memories
 	var numOne int8
     var name1 string
+    var ages[3]int=[3]int{12,14,16}
+	fmt.Println(ages,len(ages))
 	fmt.Println("enter your age")
 	fmt.Scanln(&numOne)
 	fmt.Println("enter your name")
@@ -14,5 +16,7 @@ func main(){
 	fmt.Printf("my name is %q and my age is %q \n", name1, numOne)
 	fmt.Printf("my name is of type %T\n", name1)
 	//sprintf formated string
-	
+	var str= fmt.Sprintf("your name is %v and my age is %v \n", name1, numOne)
+	fmt.Println("the saved string is", str)
+
 }
