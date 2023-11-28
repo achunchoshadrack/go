4b6fba8852049f20cd6b76f17f7e444feaeb9c6c
achunchoshadrack <achunchoshadrack@gmail.com>
2023-11-28 11:47:18 -0800

    desh

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ import "fmt"
 func main(){
 	// bits and memories
 	var numOne int8
-	fmt.Println("enter student age")
+    var name1 string
+	fmt.Println("enter your age")
 	fmt.Scanln(&numOne)
-	fmt.Println("the students ageis:", numOne)
+	fmt.Println("enter your age")
+	fmt.Scanln(&name1)
+	fmt.Println("my name is ", name1,"and my age is", numOne)
 }
