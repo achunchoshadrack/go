60aa9fe5566c08e3beb2326a885dc56d32ba4ec2
achunchoshadrack <achunchoshadrack@gmail.com>
2023-11-28 11:14:43 -0800

    desh

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main(){
+	// bits and memories
+	var numOne int8
+	fmt.Println("enter student age")
+	fmt.Scanln(&numOne)
+	fmt.Println("the students ageis:", numOne)
+}
