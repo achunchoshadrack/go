b1c7e416604088e92c44809a95bd21c8b83528be
achunchoshadrack <achunchoshadrack@gmail.com>
2023-11-29 11:31:37 -0800

    desh

--- functions.go
+++ functions.go
@@ -1,5 +1,6 @@
 package main
 import ("fmt"
+"math"
 
 )
 //basic functions with one arguments
@@ -15,9 +16,18 @@ func funcnamecy(n []string, f func(string)){
 		f(v)
 	}
 }
+// functions that return a value
+func circleArea(r float64) float64{
+	return math.Pi * r * r
+}
+
 func main(){
 	//greetings("Mac")
     // goodby("Mac")
 	funcnamecy([]string{"Paul","Mac","John"}, greetings)
     funcnamecy([]string{"Paul","Mac","John"}, goodby)
+	//calling returning value functions
+	a1:=circleArea(10.5)
+	a2:=circleArea(5.5)
+	fmt.Printf("Area of circle 1 is: %.3f\n Area of circle 2 is: %.3f",a1,a2)
 }
